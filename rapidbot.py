21da6cd4ccf13fb7373bb58d44b260ea39cd448b
rapidslayer101 <rapidslayer101@gmail.com>
2021-07-21 22:14:57 +0100

    Added back in some commands for james

--- rapidbot.py
+++ rapidbot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:113ad8765d4171cc95f3258f5482813723247b9e35b110eb42d535bbbcb7c638
-size 154986
+oid sha256:2e87e57db4660810f5bbc7b99d4ac59ef52e2b63ebeaa7820bdbea3b3807a566
+size 155448
